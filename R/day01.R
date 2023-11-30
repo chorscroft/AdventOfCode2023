147808262d97274d3304a9971562da1e66da08f2
Clare Horscroft <chorscroft@aol.co.uk>
2023-11-30 18:12:53 +0000

    init commit

---
+++ R/day01.R
@@ -0,0 +1,23 @@
+# Advent of Code Day 1
+# Part 1
+
+## Open a connection to the file
+mydata<-file("data/day01.txt","r")
+
+## Close the file connection
+close(mydata)
+
+## Read out the result
+myResult
+
+###########################
+# Part 2
+
+## Open a connection to the file
+mydata<-file("data/day01.txt","r")
+
+## Close the file connection
+close(mydata)
+
+## Read out the result
+myResult
